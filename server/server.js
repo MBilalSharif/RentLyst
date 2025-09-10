ab20cb377f5ce78618fe543cb1d800458051c502
MBilalSharif <bilalsaeedmbs@gmail.com>
2025-09-11 02:05:02 +0500

    Update project files

--- server/server.js
+++ server/server.js
@@ -24,6 +24,11 @@ app.use(express.json());
 app.use('/api/rentals', rentalRoutes);
 app.use('/api/auth', authRoutes);
 
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is live on Render!");
+});
+
 // DB + server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Atlas connected!'))
