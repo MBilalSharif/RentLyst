6b4c6bcd669ee314364ae5220276392e886349b8
MBilalSharif <bilalsaeedmbs@gmail.com>
2025-09-14 02:27:40 +0500

    your message here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,10 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-// import './index.css';
-
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Register from './components/Register';
 import RentingListings from './components/RentingListings';
-
 import LandlordRegister from './components/LandlordRegister';
 import LandlordDashboard from './components/LandlordDashboard';
 import AddRentalProperty from './components/AddRentalProperty';
@@ -28,7 +25,6 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/listings" element={<HouseListing />} />
         <Route path="/landlord-register" element={<LandlordRegister />} />
         <Route path="/LandingPage" element={<LandingPage />} />
         <Route path="/rent" element={<RentingListings />} />
